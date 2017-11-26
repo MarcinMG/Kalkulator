3ef2469ddde08bf35939377c4f7c129e72cdc48e
MarcinMG <mmgrabowski@o2.pl>
2017-11-26 12:11:47 +0100

    Second Commit

--- src/main/java/com/kalkulator/Calculator.java
+++ src/main/java/com/kalkulator/Calculator.java
@@ -1,4 +1,21 @@
 package com.kalkulator;
 
 public class Calculator {
+
+    public int addAToB(int a, int b) {
+        return a + b;
+    }
+
+    public int subAFromB(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        System.out.println(calculator.addAToB(1,2));
+        System.out.println(calculator.subAFromB(11,2));
+
+    }
 }
