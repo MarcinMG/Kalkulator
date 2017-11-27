74dbd696cd9df8de2b99b8d70b07ca78c72091c3
MarcinMG <mmgrabowski@o2.pl>
2017-11-27 18:44:11 +0100

    dodaje metode testAB

--- src/main/java/com/kalkulator/Calculator.java
+++ src/main/java/com/kalkulator/Calculator.java
@@ -10,6 +10,10 @@ public class Calculator {
         return a - b;
     }
 
+    public int testAB(int a, int b) {
+        return a * b;
+    }
+
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
